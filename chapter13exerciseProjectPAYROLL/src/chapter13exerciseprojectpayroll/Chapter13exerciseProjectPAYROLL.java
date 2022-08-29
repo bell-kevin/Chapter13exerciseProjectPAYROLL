46b4df01f840d956a2c2a2d943822d2fd481df0d
Kevin Bell <bellKevin@pm.me>
2022-08-29 14:04:49 -0600

    Add files via upload

---
+++ chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Chapter13exerciseProjectPAYROLL.java
@@ -0,0 +1,18 @@
+/** ****************************************************************************
+ * Driver.java
+ * Kevin Bell
+ *
+ * This driver program calculates employee payroll.
+ **************************************************************************** */
+package chapter13exerciseprojectpayroll;
+
+import java.util.ArrayList;
+
+public class Chapter13exerciseProjectPAYROLL {
+
+    public static void main(String[] args) {
+        System.out.println("Ch 13 Payroll by Kevin Bell");
+        ArrayList<Hourly> staff = new ArrayList<>();
+        ArrayList<Salary> managers = new ArrayList<>();
+    } // end main method
+} // end Chapter13exerciseProjectPAYROLL class
---
+++ chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Hourly.java
@@ -0,0 +1,11 @@
+/** ****************************************************************************
+ * Driver.java
+ * Kevin Bell
+ *
+ * This class manages hourly employee software for payroll.
+ **************************************************************************** */
+package chapter13exerciseprojectpayroll;
+
+class Hourly {
+    
+}
