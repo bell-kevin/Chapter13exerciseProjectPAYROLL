1d171582f2e35759544e0502cf24257efa9c1e07
Kevin Bell <bellKevin@pm.me>
2022-08-29 14:51:23 -0600

    Add files via upload

---
+++ chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Employee.java
@@ -0,0 +1,39 @@
+/** ****************************************************************************
+ * Driver.java
+ * Kevin Bell
+ *
+ * This is the base/super class for an Employee.
+ **************************************************************************** */
+package chapter13exerciseprojectpayroll;
+
+class Employee {
+
+    public String firstName = "kevin", lastName = "bell";
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    } // end constructor
+
+    public String getFirstName() {
+        return this.firstName;
+    } // end accessor method
+
+    public String getLastName() {
+        return this.lastName;
+    } // end accessor method
+
+    public String getFullName() {
+        String fullName = firstName.concat(" " + lastName);
+        System.out.println(fullName);
+        return firstName.concat(lastName);
+    } // end accessor method
+
+    void setFirstName(String firstName) {
+        this.firstName = firstName;
+    } // end mutator method
+
+    void setlastName(String lastName) {
+        this.lastName = lastName;
+    } // end mutator method
+} //end class
---
+++ chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Hourly.java
@@ -0,0 +1,14 @@
+/** ****************************************************************************
+ * Driver.java
+ * Kevin Bell
+ *
+ * This class manages hourly employee software for payroll.
+ **************************************************************************** */
+package chapter13exerciseprojectpayroll;
+
+public class Hourly extends Employee {
+
+    public Hourly(String firstName, String lastName) {
+        super(firstName, lastName);
+    } // end Hourly method
+} // end class
