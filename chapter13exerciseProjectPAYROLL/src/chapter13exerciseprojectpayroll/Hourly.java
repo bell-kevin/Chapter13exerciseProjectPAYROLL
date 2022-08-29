d39046f263c0d077d0dbb8119cfff5d00df6947f
Kevin Bell <bellKevin@pm.me>
2022-08-29 14:36:08 -0600

    Add files via upload

---
+++ chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Hourly.java
@@ -0,0 +1,10 @@
+/** ****************************************************************************
+ * Driver.java
+ * Kevin Bell
+ *
+ * This class manages hourly employee software for payroll.
+ **************************************************************************** */
+package chapter13exerciseprojectpayroll;
+
+class Hourly {
+}
---
+++ chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Salary.java
@@ -0,0 +1,11 @@
+/** ****************************************************************************
+ * Driver.java
+ * Kevin Bell
+ *
+ * This class manages salary employee software for payroll.
+ **************************************************************************** */
+package chapter13exerciseprojectpayroll;
+
+class Salary {
+
+}
