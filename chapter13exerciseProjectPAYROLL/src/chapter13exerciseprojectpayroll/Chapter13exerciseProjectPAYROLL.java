878db9da51d096b3f66f1012c25f8e053977fd5f
Kevin Bell <bellKevin@pm.me>
2022-08-30 09:52:27 -0600

    Update Chapter13exerciseProjectPAYROLL.java

--- chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Chapter13exerciseProjectPAYROLL.java
+++ chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Chapter13exerciseProjectPAYROLL.java
@@ -7,10 +7,12 @@
 package chapter13exerciseprojectpayroll;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Chapter13exerciseProjectPAYROLL {
 
     public static void main(String[] args) {
+        Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.println("Ch 13 Payroll by Kevin Bell");
         ArrayList<Hourly> staff = new ArrayList<>();
         ArrayList<Salary> managers = new ArrayList<>();
@@ -19,5 +21,8 @@ public class Chapter13exerciseProjectPAYROLL {
         staff.add(new Hourly("Dan", "Peters"));
         managers.add(new Salary("Ann", "Barrett"));
         managers.add(new Salary("Frank", "Greene"));
+        for (int i = 0; i < 3; i++) {
+            System.out.println("How many hours for employee " + null);
+        } // end for loop
     } // end main method
 } // end Chapter13exerciseProjectPAYROLL class
