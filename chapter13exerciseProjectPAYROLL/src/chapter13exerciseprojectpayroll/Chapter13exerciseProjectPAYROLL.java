b8abca21d7e8164e9445358963a8ee96ab9d712c
Kevin Bell <bellKevin@pm.me>
2022-08-30 10:40:29 -0600

    Update Chapter13exerciseProjectPAYROLL.java

--- chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Chapter13exerciseProjectPAYROLL.java
+++ chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Chapter13exerciseProjectPAYROLL.java
@@ -13,7 +13,7 @@ public class Chapter13exerciseProjectPAYROLL {
 
     public static void main(String[] args) {
         Scanner computerKeyboardInput = new Scanner(System.in);
-        System.out.println("Ch 13 Payroll by Kevin Bell");
+        System.out.println("Ch 13 Payroll by Kevin Bell \n");
         ArrayList<Hourly> staff = new ArrayList<>();
         ArrayList<Salary> managers = new ArrayList<>();
         staff.add(new Hourly("Barry", "Johnson", 12.50));
@@ -21,8 +21,12 @@ public class Chapter13exerciseProjectPAYROLL {
         staff.add(new Hourly("Dan", "Peters", 10.00));
         managers.add(new Salary("Ann", "Barrett", 52000));
         managers.add(new Salary("Frank", "Greene", 48000));
-        for (Hourly emp : staff) {
-            System.out.println("How many hours for employee " + emp.getFullName()+": ");   
+        for (Hourly hourly : staff) {
+            System.out.print("How many hours for employee " + hourly.getFullName()+": ");
+            double hours = computerKeyboardInput.nextDouble();
         } // end for-each loop
+        System.out.println("");
+        System.out.println("Payroll Results \n");
+        
     } // end main method
 } // end Chapter13exerciseProjectPAYROLL class
