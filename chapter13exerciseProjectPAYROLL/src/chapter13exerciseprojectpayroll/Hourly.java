25640fc3c36215e35cc45585e26132aeee338d67
Kevin Bell <bellKevin@pm.me>
2022-08-29 20:35:28 -0600

    Update Hourly.java

--- chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Hourly.java
+++ chapter13exerciseProjectPAYROLL/src/chapter13exerciseprojectpayroll/Hourly.java
@@ -1,5 +1,5 @@
 /** ****************************************************************************
- * Driver.java
+ * Hourly.java
  * Kevin Bell
  *
  * This class manages hourly employee software for payroll.
@@ -8,7 +8,40 @@ package chapter13exerciseprojectpayroll;
 
 public class Hourly extends Employee {
 
+    double payRate, hours, overTimeHours, overTimePayRate;
+    private String firstName;
+    private String lastName;
+
     public Hourly(String firstName, String lastName) {
         super(firstName, lastName);
-    } // end Hourly method
+    } // end constructor method
+
+    public Hourly(String firstName, String lastName, double payRate) {
+        super(firstName, lastName);
+        this.payRate = payRate;
+    } // end constructor method
+
+    public double getPayRate() {
+        return this.payRate;
+    } // end accessor method
+
+    public double getHours() {
+        return this.hours;
+    } // end accessor method
+
+    void setPayRate(double payRate) {
+        this.payRate = payRate;
+    } // end mutator method
+
+    void setHours(double hours) {
+        this.hours = hours;
+    } // end mutator method 
+
+    public double grossPay(double hours, double payRate) {
+        if (hours > 40) {
+            overTimePayRate = payRate * 1.5;
+        } // end if condition
+        double grossPay = (hours * payRate) + (overTimeHours * overTimePayRate);
+        return grossPay;
+    } // end grossPay method
 } // end class
