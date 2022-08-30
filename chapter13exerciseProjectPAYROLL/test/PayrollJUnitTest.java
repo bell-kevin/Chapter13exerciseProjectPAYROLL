fb8f3aa1a9e69122510be73a1305b9c1fe6c302f
Kevin Bell <bellKevin@pm.me>
2022-08-29 20:34:42 -0600

    Update PayrollJUnitTest.java

--- chapter13exerciseProjectPAYROLL/test/PayrollJUnitTest.java
+++ chapter13exerciseProjectPAYROLL/test/PayrollJUnitTest.java
@@ -1,44 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+/** ****************************************************************************
+ * PayrollJUnitTest.java
+ * Kevin Bell
+ *
+ * This is the unit testing class.
+ **************************************************************************** */
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author 4800590195
- */
 public class PayrollJUnitTest {
 
     public PayrollJUnitTest() {
     }
 
-    @BeforeClass
-    public static void setUpClass() {
-    }
+    @Test
+    public void weeklyPay1() {
+        System.out.println("weeklyPayTest1");
+        double annualPayRate = 52000;
+        double weeksPerYear = 52;
+        double expectedResult = 1000;
+        double result = (annualPayRate / weeksPerYear);
+        double delta = 0;
+        assertEquals(expectedResult, result, delta);
+    } // end weeklyPay1 test method
 
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    @Test
+    public void weeklyPay2() {
+        System.out.println("weeklyPayTest2");
+        double annualPayRate = 52052;
+        double weeksPerYear = 52;
+        double expectedResult = 1001;
+        double result = (annualPayRate / weeksPerYear);
+        double delta = 0;
+        assertEquals(expectedResult, result, delta);
+    } // end weeklyPay2 test method
 
-    @Before
-    public void setUp() {
-    }
+    @Test
+    public void weeklyPay3() {
+        System.out.println("weeklyPayTest3");
+        double annualPayRate = 52104;
+        double weeksPerYear = 52;
+        double expectedResult = 1002;
+        double result = (annualPayRate / weeksPerYear);
+        double delta = 0;
+        assertEquals(expectedResult, result, delta);
+    } // end weeklyPay3 test method
 
-    @After
-    public void tearDown() {
-    }
+    @Test
+    public void weeklyPay4() {
+        System.out.println("weeklyPayTest4");
+        double annualPayRate = 52156;
+        double weeksPerYear = 52;
+        double expectedResult = 1003;
+        double result = (annualPayRate / weeksPerYear);
+        double delta = 0;
+        assertEquals(expectedResult, result, delta);
+    } // end weeklyPay4 test method
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
-}
+    @Test
+    public void weeklyPay5() {
+        System.out.println("weeklyPayTest5");
+        double annualPayRate = 52208;
+        double weeksPerYear = 52;
+        double expectedResult = 1004;
+        double result = (annualPayRate / weeksPerYear);
+        double delta = 0;
+        assertEquals(expectedResult, result, delta);
+    } // end weeklyPay5 test method
+} // end unit testing class
